0981ebfab54dcfb8fe0090f6789dc0a2c573a7d6
Antonio Vanegas <hpsaturn@gmail.com>
2018-06-22 18:16:55 -0500

    add service on background for BLE functions

---
+++ app/src/main/java/hpsaturn/pollutionreporter/Config.java
@@ -0,0 +1,12 @@
+package hpsaturn.pollutionreporter;
+
+/**
+ * Created by Antonio Vanegas @hpsaturn on 6/22/18.
+ */
+public class Config {
+
+    // Record Service whatchdog polling
+    public static final long DEFAULT_INTERVAL = 15 * 1000;  // seconds
+    public static final int TIME_AFTER_START = 5; // after boot starting time
+
+}
