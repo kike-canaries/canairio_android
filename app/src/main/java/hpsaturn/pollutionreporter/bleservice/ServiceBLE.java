588da05af7a232cef9b76af79e327d95c349d2dd
Antonio Vanegas <hpsaturn@gmail.com>
2018-07-23 19:34:03 -0500

    temporal workaround for API26 startService crash (added notification)

--- app/src/main/java/hpsaturn/pollutionreporter/bleservice/ServiceBLE.java
+++ app/src/main/java/hpsaturn/pollutionreporter/bleservice/ServiceBLE.java
@@ -1,9 +1,14 @@
 package hpsaturn.pollutionreporter.bleservice;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.gson.Gson;
 import com.hpsaturn.tools.Logger;
@@ -31,31 +36,43 @@ public class ServiceBLE extends Service {
 
     private ArrayList<SensorData> buffer = new ArrayList<>();
 
-    private final int RETRY_POLICY   = 5;
-    private int retry_connect        = 0;
-    private int retry_notify_setup   = 0;
+    private final int RETRY_POLICY = 5;
+    private int retry_connect = 0;
+    private int retry_notify_setup = 0;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Logger.i(TAG,"[BLE] Creating Service container..");
+        Logger.i(TAG, "[BLE] Creating Service container..");
         prefBuilder = AppData.getPrefBuilder(this);
-        isRecording=prefBuilder.getBoolean(Keys.SENSOR_RECORD,false);
-        serviceManager = new ServiceManager(this,managerListener);
+        isRecording = prefBuilder.getBoolean(Keys.SENSOR_RECORD, false);
+        serviceManager = new ServiceManager(this, managerListener);
+        noticationChannelAPI26issue();
     }
 
-    private void startConnection(){
-        if (prefBuilder.getBoolean(Keys.DEVICE_PAIR, false)) {
-            if(bleHandler==null) {
-                connect();
-            }
-            else if(bleHandler.isConnected())Logger.i(TAG,"[BLE] already connected!");
-            else connect();
+    private void noticationChannelAPI26issue() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "preporter";
+            String CHANNEL_NAME = "PollutionReporter";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_LOW);
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("")
+                    .setContentText("").build();
+            startForeground(1, notification);
         }
-        else Logger.w(TAG,"[BLE] not BLE connection (not MAC register)");
     }
 
-    private void connect(){
+    private void startConnection() {
+        if (prefBuilder.getBoolean(Keys.DEVICE_PAIR, false)) {
+            if (bleHandler == null) {
+                connect();
+            } else if (bleHandler.isConnected()) Logger.i(TAG, "[BLE] already connected!");
+            else connect();
+        } else Logger.w(TAG, "[BLE] not BLE connection (not MAC register)");
+    }
+
+    private void connect() {
         Logger.i(TAG, "[BLE] starting BLE connection..");
         String macAddress = prefBuilder.getString(Keys.DEVICE_ADDRESS, "");
         Logger.i(TAG, "[BLE] deviceConnect to " + macAddress);
@@ -65,9 +82,9 @@ public class ServiceBLE extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Logger.d(TAG,"[BLE] onStartCommand");
+        Logger.d(TAG, "[BLE] onStartCommand");
         serviceManager.status(ServiceManager.STATUS_SERVICE_OK);
-        if(isRecording)startConnection();
+        if (isRecording) startConnection();
         return START_STICKY;
     }
 
@@ -84,15 +101,15 @@ public class ServiceBLE extends Service {
 
         @Override
         public void onServiceStart() {
-            Logger.d(TAG,"[BLE] request service start..");
+            Logger.d(TAG, "[BLE] request service start..");
             startConnection();
         }
 
         @Override
         public void onServiceStop() {
-            Logger.w(TAG,"[BLE] request service stop..");
-            if(bleHandler!=null&&!isRecording)bleHandler.triggerDisconnect();
-            else if (isRecording) Logger.w(TAG,"[BLE] isRecording override stop BLE service");
+            Logger.w(TAG, "[BLE] request service stop..");
+            if (bleHandler != null && !isRecording) bleHandler.triggerDisconnect();
+            else if (isRecording) Logger.w(TAG, "[BLE] isRecording override stop BLE service");
         }
 
         @Override
@@ -102,12 +119,12 @@ public class ServiceBLE extends Service {
 
         @Override
         public void onSensorRecord() {
-           isRecording=true;
+            isRecording = true;
         }
 
         @Override
         public void onSensorRecordStop() {
-            isRecording=false;
+            isRecording = false;
         }
     };
 
@@ -120,16 +137,16 @@ public class ServiceBLE extends Service {
         @Override
         public void onConnectionSuccess() {
             serviceManager.status(ServiceManager.STATUS_BLE_START);
-            retry_connect=0;
+            retry_connect = 0;
         }
 
         @Override
         public void onConectionFailure() {
-           serviceManager.status(ServiceManager.STATUS_BLE_FAILURE);
-           if(retry_connect++<RETRY_POLICY) {
-               Logger.w(TAG,"[BLE] retry connection on failure.."+retry_connect);
-               startConnection();
-           }
+            serviceManager.status(ServiceManager.STATUS_BLE_FAILURE);
+            if (retry_connect++ < RETRY_POLICY) {
+                Logger.w(TAG, "[BLE] retry connection on failure.." + retry_connect);
+                startConnection();
+            }
         }
 
         @Override
@@ -144,33 +161,33 @@ public class ServiceBLE extends Service {
 
         @Override
         public void onNotificationSetupFailure() {
-            Logger.e(TAG,"[BLE] onNotificationSetupFailure");
-            if(retry_notify_setup++<RETRY_POLICY) {
-                Logger.w(TAG,"[BLE] retry notify setup.."+retry_notify_setup);
+            Logger.e(TAG, "[BLE] onNotificationSetupFailure");
+            if (retry_notify_setup++ < RETRY_POLICY) {
+                Logger.w(TAG, "[BLE] retry notify setup.." + retry_notify_setup);
                 bleHandler.setupNotification();
             }
         }
 
         @Override
         public void onNotificationReceived(byte[] bytes) {
-            if(isRecording)record(bytes);
-            Logger.d(TAG,"[BLE] pushing data..");
+            if (isRecording) record(bytes);
+            Logger.d(TAG, "[BLE] pushing data..");
             serviceManager.pushData(bytes);
-            retry_notify_setup=0;
+            retry_notify_setup = 0;
         }
     };
 
-    private void record(byte[] bytes){
+    private void record(byte[] bytes) {
         String strdata = new String(bytes);
         Logger.i(TAG, "[BLE] data to record: " + strdata);
         SensorData item = new Gson().fromJson(strdata, SensorData.class);
         buffer.add(item);
-        if(buffer.size()>10){
+        if (buffer.size() > 10) {
             Logger.i(TAG, "[BLE] saving buffer..");
             ArrayList<SensorData> data = Storage.getData(this);
             data.addAll(buffer);
-            Logger.i(TAG, "[BLE] data size: "+data.size());
-            Storage.setData(this,data);
+            Logger.i(TAG, "[BLE] data size: " + data.size());
+            Storage.setData(this, data);
             buffer.clear();
             Logger.i(TAG, "[BLE] saving buffer done.");
         }
