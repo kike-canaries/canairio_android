f7dfef5a15d85be7bb68f32262b0bc6ca4dcd7d7
Antonio Vanegas <hpsaturn@gmail.com>
2018-08-16 20:59:27 -0500

    fixed crash on some situations and added getTrack feature

--- app/src/main/java/hpsaturn/pollutionreporter/common/Storage.java
+++ app/src/main/java/hpsaturn/pollutionreporter/common/Storage.java
@@ -40,13 +40,18 @@ public class Storage {
     }
 
     public static ArrayList<SensorTrack> getTracks(Context ctx) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ctx);
-        String ringJson = preferences.getString(Keys.SENSOR_TRACKS, "");
-        if (ringJson.equals("")) return new ArrayList<>();
-        else {
-            Type listType = new TypeToken<ArrayList<SensorTrack>>() {
-            }.getType();
-            return new Gson().fromJson(ringJson, listType);
+        try {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ctx);
+            String ringJson = preferences.getString(Keys.SENSOR_TRACKS, "");
+            if (ringJson.equals("")) return new ArrayList<>();
+            else {
+                Type listType = new TypeToken<ArrayList<SensorTrack>>() {
+                }.getType();
+                return new Gson().fromJson(ringJson, listType);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
         }
     }
 
@@ -64,6 +69,16 @@ public class Storage {
             if(track.name.equals(id))it.remove();
         }
         saveTracks(ctx,tracks);
+    }
+
+    public static SensorTrack getTrack(Context ctx, String id){
+        ArrayList<SensorTrack> tracks = getTracks(ctx);
+        Iterator<SensorTrack> it = tracks.iterator();
+        while(it.hasNext()){
+            SensorTrack track = it.next();
+            if(track.name.equals(id))return track;
+        }
+        return null;
     }
 
     private static void saveTracks(Context ctx,ArrayList<SensorTrack>tracks){
