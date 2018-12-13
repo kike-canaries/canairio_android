3bacd20af1664f2e908f26572dfc1691360bbc1d
Antonio Vanegas <hpsaturn@gmail.com>
2018-12-13 22:18:22 +0100

    update tools and gradle, fixed somme cmake issues

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,10 @@
-#include <jni.h>
 #include <string>
+#include <jni.h>
 
 extern "C" JNIEXPORT jstring
 
 JNICALL
 Java_hpsaturn_pollutionreporter_BaseActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+  std::string hello = "Hello from C++";
+  return env->NewStringUTF(hello.c_str());
 }
