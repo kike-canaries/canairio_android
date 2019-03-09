d248f355077ce4aaa56d3729678e7e84ebbff83b
Antonio Vanegas <hpsaturn@gmail.com>
2019-03-10 00:33:57 +0100

    updated summary fields via config BLE callback

--- app/src/main/java/hpsaturn/pollutionreporter/view/SettingsFragment.java
+++ app/src/main/java/hpsaturn/pollutionreporter/view/SettingsFragment.java
@@ -2,6 +2,8 @@ package hpsaturn.pollutionreporter.view;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
@@ -38,13 +40,17 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
         updateStimeSummary();
         updateWifiSwitch();
-        
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getMain().getServiceManager().readSensorConfig();
     }
 
     @Override
@@ -139,7 +145,24 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
             Logger.i(TAG, "ssid: " + config.ssid);
             Logger.i(TAG, "stime: " + config.stime);
             getMain().showSnackMessage(R.string.msg_config_saved);
+            updatePreferencesSummmary(config);
         }
+    }
+
+    private void updatePreferencesSummmary(SensorConfig config) {
+        Preference pref;
+        pref = findPreference(getString(R.string.key_setting_ssid));
+        pref.setSummary(config.ssid);
+        pref = findPreference(getString(R.string.key_setting_ifxdb));
+        pref.setSummary(config.ifxdb);
+        pref = findPreference(getString(R.string.key_setting_ifxip));
+        pref.setSummary(config.ifxip);
+        pref = findPreference(getString(R.string.key_setting_ifxid));
+        pref.setSummary(config.ifxid);
+        pref = findPreference(getString(R.string.key_setting_ifxtg));
+        pref.setSummary(config.ifxtg);
+        pref = findPreference(getString(R.string.key_setting_stime));
+        pref.setSummary("" + config.stime + " seconds");
     }
 
     public void saveSharedPreference(String key, String value) {
