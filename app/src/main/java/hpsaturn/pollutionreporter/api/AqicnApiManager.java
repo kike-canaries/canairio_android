b92a3c2a39edf4fdfb51017a6a388e3ac1fe587b
Antonio Vanegas <hpsaturn@gmail.com>
2020-01-04 22:56:43 +0100

    refactor API key migrated to API init

--- app/src/main/java/hpsaturn/pollutionreporter/api/AqicnApiManager.java
+++ app/src/main/java/hpsaturn/pollutionreporter/api/AqicnApiManager.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.hpsaturn.tools.Logger;
+
+import hpsaturn.pollutionreporter.R;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
@@ -22,6 +24,7 @@ public class AqicnApiManager {
     private static AqicnApiManager instance;
     private Context mContext;
     private AqicnInterface service;
+    private static String API_KEY;
 
     public static AqicnApiManager getInstance() {
         if (instance == null) {
@@ -32,6 +35,7 @@ public class AqicnApiManager {
 
     public void init(Context context) {
         mContext = context;
+        API_KEY=context.getString(R.string.api_aqicn_key);
 
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").create();
 
@@ -45,18 +49,18 @@ public class AqicnApiManager {
         service = retrofit.create(AqicnInterface.class);
     }
 
-    public void getDataFromCity (String token, String city, Callback <AqicnDataResponse> callback){
-        Call<AqicnDataResponse> call = service.getDataFromCity(city,token);
+    public void getDataFromCity(String city, Callback<AqicnDataResponse> callback){
+        Call<AqicnDataResponse> call = service.getDataFromCity(API_KEY,city);
         call.enqueue(callback);
     }
 
-    public void getDataFromHere (String token, Callback <AqicnDataResponse> callback){
-        Call<AqicnDataResponse> call = service.getDataFromHere(token);
+    public void getDataFromHere(Callback<AqicnDataResponse> callback){
+        Call<AqicnDataResponse> call = service.getDataFromHere(API_KEY);
         call.enqueue(callback);
     }
 
-    public void getDataFromMapBounds (String token, String latlng, Callback <AqicnDataResponse> callback){
-        Call<AqicnDataResponse> call = service.getDataFromMapBounds(token,latlng);
+    public void getDataFromMapBounds(String latlng, Callback<AqicnDataResponse> callback){
+        Call<AqicnDataResponse> call = service.getDataFromMapBounds(API_KEY,latlng);
         call.enqueue(callback);
     }
 
