bc787fbc1c9adc41e4c837036e5aeb6c4413f8b1
Juan Pablo <juan.pa097@gmail.com>
2020-06-03 19:04:22 -0500

    feat: created material design colors and text styles

---
+++ app/src/main/java/hpsaturn/pollutionreporter/dashboard/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package hpsaturn.pollutionreporter.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hpsaturn.pollutionreporter.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
