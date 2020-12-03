9a278139d4d19db0340d2be7dc961839309d79c7
Juan Pablo Peñaloza <juan-penaloza@javeriana.edu.co>
2020-12-03 17:52:58 -0500

    fix: Added spinner to public records list.

--- app/src/main/java/hpsaturn/pollutionreporter/reports/open/presentation/OpenSensorReportsViewModel.kt
+++ app/src/main/java/hpsaturn/pollutionreporter/reports/open/presentation/OpenSensorReportsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import hpsaturn.pollutionreporter.core.domain.entities.InProgress
 import hpsaturn.pollutionreporter.core.domain.entities.Result
 import hpsaturn.pollutionreporter.reports.open.domain.entities.SensorReportInformation
 import hpsaturn.pollutionreporter.reports.open.domain.usecases.LoadOpenSensorReports
@@ -12,6 +13,7 @@ class OpenSensorReportsViewModel @ViewModelInject constructor(
     private val loadOpenSensorReports: LoadOpenSensorReports
 ) : ViewModel() {
     val publicReports: LiveData<Result<List<SensorReportInformation>>> = liveData {
+        emit(InProgress)
         emit(loadOpenSensorReports())
     }
 }
