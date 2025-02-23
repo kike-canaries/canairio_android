ade10392c9c3d7fc4c6fcaba54de19962ca4e3c4
Hpsaturn <hpsaturn@gmail.com>
2025-02-23 11:56:16 +0100

    rev625 v0.9.4 fixed possible crash on service receiver launcher

--- app/src/main/java/hpsaturn/pollutionreporter/service/RecordTrackReceiver.java
+++ app/src/main/java/hpsaturn/pollutionreporter/service/RecordTrackReceiver.java
@@ -1,16 +1,11 @@
 package hpsaturn.pollutionreporter.service;
 
-import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
-import androidx.core.content.ContextCompat;
 
 import com.hpsaturn.tools.Logger;
-
-import hpsaturn.pollutionreporter.PermissionUtil;
 
 /**
  * Created by Antonio Vanegas @hpsaturn on 3/24/17.
@@ -24,21 +19,13 @@ public class RecordTrackReceiver extends BroadcastReceiver {
         try {
             Logger.d(TAG, "[BLE] StartServiceReceiver: onReceive..");
             Intent service = new Intent(context, RecordTrackService.class);
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-//                    String permission = Manifest.permission.ACCESS_BACKGROUND_LOCATION;
-//                    int res = context.checkCallingOrSelfPermission(permission);
-//                    boolean cp_bg_loc = (res == PackageManager.PERMISSION_GRANTED);
-//                    if (!cp_bg_loc) Logger.w(TAG, "[BLE] StartServiceReceiver: FAILED!");
-//                    if (!cp_bg_loc) return;
-//                }
-//                ContextCompat.startForegroundService(context, service);
-//            } else {
-//                context.startService(service);
-//            }
-//            if (PermissionUtil.hasForegroundLocationPermission(context)).
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(service);
+            } else {
                 context.startService(service);
+            }
         } catch (Exception e) {
+            Logger.w(TAG, "[BLE] StartServiceReceiver: FAILED!");
             throw new RuntimeException(e);
         }
     }
