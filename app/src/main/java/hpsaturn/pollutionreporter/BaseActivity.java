1ce2fcc85c1711e7a9ceca78e3ed445329832990
Hpsaturn <hpsaturn@gmail.com>
2025-11-22 15:03:47 +0100

    updated and refactored to support API 36

--- app/src/main/java/hpsaturn/pollutionreporter/BaseActivity.java
+++ app/src/main/java/hpsaturn/pollutionreporter/BaseActivity.java
@@ -300,16 +300,4 @@ public abstract class BaseActivity extends AppCompatActivity {
     abstract void menuActionVarFilter();
     abstract void menuActionShowAbout();
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
 }
